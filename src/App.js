833297d4aa752c8a25529538a26bd0a147bbc0b9
AlexA2300 <alar2300@colorado.edu>
2023-06-13 16:54:24 -0600

    Implementation of Component Files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+// React Imports
+import React from 'react';
+import { ReactDOM } from 'react';
+
+// Local Imports
+import Playlist from './components/Playlist';
+import SearchBar from './components/SearchBar';
+import SearchResults from './components/SearchResults';
+import Track from './components/Track';
+import Tracklist from './components/Tracklist';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello World</h1>
   );
 }
 
