62c2c14825fbcf2ba151187c18b7ebf6729d5221
AlexA2300 <alar2300@colorado.edu>
2023-06-15 15:15:20 -0500

    Final update of file structure

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,7 +1,1 @@
-import React from 'react';
 
-function Playlist() {
-
-};
-
-export default Playlist;
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,1 @@
-import React from 'react';
 
-function SearchBar() {
-
-};
-
-export default SearchBar;
