9541d00f93e97d3957ab0897a52159f82ff16ce4
AlexA2300 <alar2300@colorado.edu>
2023-06-16 13:16:08 -0500

    Implementation of SearchBar functionality

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,1 +1,26 @@
+import React, { useState, useCallback } from 'react';
 
+import './SearchBar.css';
+
+const SearchBar = props => {
+
+    const [term, setTerm] = useState("");
+
+    const handleTermChange = useCallback(
+        event => {
+            setTerm(event.target.value);
+        },
+        []
+    );
+
+    return (
+        <div className='SearchBar'>
+            <input placeholder="Enter A Song Title" onChange={handleTermChange} />
+            <button className='SearchButton' onClick={search}>
+                SEARCH
+            </button>
+        </div>
+    );
+};
+
+export default SearchBar;
